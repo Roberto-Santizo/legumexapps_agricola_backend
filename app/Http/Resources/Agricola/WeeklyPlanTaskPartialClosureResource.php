f90398bb209855724d07e4307bcd2b64192cef79
Roberto Santizo <soportetecnico.tejar@legumex.net>
2026-07-16 10:39:32 -0600

    patch: weekly plan task resource

--- app/Http/Resources/Agricola/WeeklyPlanTaskPartialClosureResource.php
+++ app/Http/Resources/Agricola/WeeklyPlanTaskPartialClosureResource.php
@@ -18,11 +18,15 @@ class WeeklyPlanTaskPartialClosureResource extends JsonResource
         $end_date = $this->end_date;
 
         return [
-            'id' =>             $this->id,
-            'start_date' =>     $start_date->format('Y-m-d'),
-            'start_time' =>     $start_date->format('h:i:s'),
-            'end_date' =>       $end_date ? $end_date->format('Y-m-d') : '',
-            'end_time' =>       $end_date ? $end_date->format('h:i:s'): '',
+            'id' =>                         $this->id,
+            'start_date' =>                 $start_date->format('Y-m-d'),
+            'start_date_string' =>          $start_date->format('d-m-Y'),
+            'start_time' =>                 $start_date->format('H:i:s'),
+            'start_time_string' =>          $start_date->format('h:i:s A'),
+            'end_date' =>                   $end_date ? $end_date->format('Y-m-d') : '',
+            'end_date_string' =>            $end_date ? $end_date->format('d-m-Y') : '',
+            'end_time' =>                   $end_date ? $end_date->format('H:i:s'): '',
+            'end_time_string' =>            $end_date ? $end_date->format('h:i:s A'): '',
         ];
     }
 }
--- app/Http/Resources/Agricola/WeeklyPlanTaskResource.php
+++ app/Http/Resources/Agricola/WeeklyPlanTaskResource.php
@@ -17,13 +17,16 @@ class WeeklyPlanTaskResource extends JsonResource
     {
         $start_date = $this->start_date;
         $end_date = $this->end_date;
+        $totalEmployees = $this->employees()->count();
         $operation_date = $this->operation_date;
         $isFinished = $start_date && $end_date;
         $isInProgress = $start_date && !$end_date;
         $total_hours = $isFinished ? round($start_date->diffInHours($end_date), 2) : ($isInProgress ? round($start_date->diffInHours(Carbon::now())) : 0);
         $isPaused = $this->partialClosures()->where('end_date', null)->first();
         $status = $isFinished ? 4 : ($isPaused ? 3 : ($isInProgress ? 2 : 1));
-
+        $partialClosures = $this->partialClosures()->whereNotNull('end_date')->get();
+        $partialClosureHours = $this->getPartialClosuresHours($partialClosures);
+        
         return [
             'id' =>                         $this->id,
             'budget' =>                     $this->budget,
@@ -38,7 +41,7 @@ class WeeklyPlanTaskResource extends JsonResource
             'finca_group_id' =>             $this->finca_group_id,
             'group' =>                      $this->group ? $this->group->code : 'Sin grupo asociado',
             'use_dron' =>                   $this->use_dron,
-            'total_hours' =>                $total_hours * $this->employees()->count(),
+            'total_hours' =>                ($total_hours * $totalEmployees) - ($partialClosureHours * $totalEmployees),
             'operation_date' =>             $operation_date ? $this->operation_date->format('Y-m-d') : null,
             'start_date' =>                 $start_date ? $start_date->format('Y-m-d') : null,
             'start_hour' =>                 $start_date ? $start_date->format('h:i:s') : null,
@@ -46,5 +49,17 @@ class WeeklyPlanTaskResource extends JsonResource
             'end_hour' =>                   $end_date ? $end_date->format('h:i:s') : null,
             'status' =>                     $status,
         ];
+    }
+
+    private function getPartialClosuresHours(mixed $partialClosures)
+    {
+        $total_hours = 0;
+
+        $partialClosures->map(function($closure) use(&$total_hours) {
+            $closureHours = $closure->start_date->diffInHours($closure->end_date);
+            $total_hours += $closureHours;
+        });
+
+        return $total_hours;    
     }
 }
